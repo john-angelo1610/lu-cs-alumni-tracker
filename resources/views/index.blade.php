5ee8486f0bb942b951c00aa1e479596fa156c459
John Angelo <67223472+john-angelo1610@users.noreply.github.com>
2022-03-31 09:58:17 +0800

    Edited the layout of the news and announcement section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,22 +5,16 @@
         <h2>Computer Science Alumni Tracker</h2>
     </section>
     <section id="news" class="container my-5">
-        <h1 class="text-center">News and Announcements</h1>
-        <div class="row">
-            @foreach ($posts as $post)
-            <div class="col-lg-4 my-4">
-                <article class="card">
-                    <h5 class="text-center card-header">{{$post->title}}</h5>
-                    <div class="card-body">
-                        <p class="card-text">{{substr($post->content, 0, 200)}}</p>
-                        <a href="/{{$post->id}}" class="btn text-light rounded-0 bg-darkgreen">Read More</a>
-                    </div>
-                    <div class="card-footer text-light text-center bg-maingreen">
-                        {{Carbon\Carbon::parse($post->date)->format('F d, Y')}}
-                    </div>
-                </article>
-            </div>
-            @endforeach
-        </div>
+        <a href="/posts" class="btn btn-outline-dark ms-auto">Manage Post</a>
+        <h1 class="text-center my-5">News and Announcements</h1>
+        @foreach ($posts as $post)
+            <h2>{{$post->title}}</h2>
+            <small>{{Carbon\Carbon::parse($post->date)->format('F d, Y')}}</small>
+            <p>{{strlen($post->content) > 200 ? substr_replace($post->content, "...", 295) : $post->content}}</p>
+            <a href="/{{$post->id}}" class="btn text-light rounded-0 bg-darkgreen mt-2">Read More</a>
+            <br>
+            <hr>
+            <br>
+        @endforeach
     </section>
 @endsection
