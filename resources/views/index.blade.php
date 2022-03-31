d611671b46328c09fa8d5ceeb076afff5f0ea5d9
John Angelo <67223472+john-angelo1610@users.noreply.github.com>
2022-03-31 10:16:44 +0800

    Fixed rendering the HTML output that came from textarea

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
         @foreach ($posts as $post)
             <h2>{{$post->title}}</h2>
             <small>{{Carbon\Carbon::parse($post->date)->format('F d, Y')}}</small>
-            <p>{{strlen($post->content) > 200 ? substr_replace($post->content, "...", 295) : $post->content}}</p>
+            <p>{{strlen(strip_tags($post->content)) > 200 ? substr_replace(strip_tags($post->content), "...", 295) : strip_tags($post->content)}}</p>
             <a href="/{{$post->id}}" class="btn text-light rounded-0 bg-darkgreen mt-2">Read More</a>
             <br>
             <hr>
